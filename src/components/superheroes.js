300cf360ddae0ef35c592193e7cd4b758853067b
Hollis <hollisjamison@gmail.com>
2021-09-09 19:27:33 -0400

    Code as of end of Week 15 - Day 2

--- src/components/superheroes.js
+++ src/components/superheroes.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Superheroes = () => {
-return (
-  <div className="page">
-    <div className="title">Meet the Heroes</div>
-    <div className="subtitle">A searchable list of all your favorite heroes</div>
-  </div>
-)
+export default () => {
+  const [slug, setSlug] = useState('')
+
+  return (
+    <div className="page">
+      <div className="title">Meet the Heroes</div>
+      <div className="subtitle">A searchable list of all your favorite heroes</div>
+      <input type="text" name="search" onChange={event => setSlug(event.target.value)} />
+      <div className="slug">{slug}</div>
+    </div>
+  )
 }
-
-export default Superheroes;

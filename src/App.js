5705a0bda1f9e45a12b986d4c93248b81dbd3e38
Hollis <hollisjamison@gmail.com>
2021-09-09 19:23:55 -0400

    Commit for end of Week 15 Day 1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Superheroes from './components/superheroes'
+
+const App = () => {
+	return (
+		<Superheroes />
+	)
+}
+
+export default App
---
+++ src/components/superheroes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Superheroes = () => {
+return (
+  <div className="page">
+    <div className="title">Meet the Heroes</div>
+    <div className="subtitle">A searchable list of all your favorite heroes</div>
+  </div>
+)
+}
+
+export default Superheroes;
